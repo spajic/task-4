27a580dc713e321ff93b4b078329039d5ecfca6a
Anna Selivanova <foxy.eyed@gmail.com>
2019-04-13 22:01:38 +0400

    Import optimization (wip)

--- app/services/db_importer.rb
+++ app/services/db_importer.rb
@@ -1,34 +1,59 @@
+require 'oj'
+
 class DbImporter
   def call(source:)
-    json = JSON.parse(File.read(source))
+    json = Oj.load_file(source)
 
     ActiveRecord::Base.transaction do
-      City.delete_all
-      Bus.delete_all
-      Service.delete_all
-      Trip.delete_all
-      ActiveRecord::Base.connection.execute('delete from buses_services;')
+      clear_db!
+      create_from_json!(json)
+    end
+  end
 
-      json.each do |trip|
-        from = City.find_or_create_by(name: trip['from'])
-        to = City.find_or_create_by(name: trip['to'])
-        services = []
-        trip['bus']['services'].each do |service|
-          s = Service.find_or_create_by(name: service)
-          services << s
-        end
-        bus = Bus.find_or_create_by(number: trip['bus']['number'])
-        bus.update(model: trip['bus']['model'], services: services)
+  def clear_db!
+    City.delete_all
+    Bus.delete_all
+    Service.delete_all
+    Trip.delete_all
+    ActiveRecord::Base.connection.execute('delete from buses_services;')
+  end
 
-        Trip.create!(
-          from: from,
-          to: to,
-          bus: bus,
-          start_time: trip['start_time'],
-          duration_minutes: trip['duration_minutes'],
-          price_cents: trip['price_cents']
-        )
+  def create_from_json!(json)
+    cities = Set.new
+    services = Set.new
+
+    json.each do |trip|
+      cities << trip['from']
+      cities << trip['to']
+
+      trip['bus']['services'].each do |service|
+        services << service
       end
+    end
+
+    cities.each do |city|
+      City.create(name: city)
+    end
+
+    services.each do |service|
+      Service.create(name: service)
+    end
+
+    json.each do |trip|
+      from = City.find_by(name: trip['from'])
+      to = City.find_by(name: trip['to'])
+      services = Service.where(name: trip['bus']['services'])
+      bus = Bus.find_or_create_by(number: trip['bus']['number'])
+      bus.update(model: trip['bus']['model'], services: services)
+
+      Trip.create!(
+        from: from,
+        to: to,
+        bus: bus,
+        start_time: trip['start_time'],
+        duration_minutes: trip['duration_minutes'],
+        price_cents: trip['price_cents']
+      )
     end
   end
 end
