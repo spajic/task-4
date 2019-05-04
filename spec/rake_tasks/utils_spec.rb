887b42325102a9282459c1adbd9a269b1aa471e7
Stanislav Kravchenko <stankra@yandex.ru>
2019-05-04 19:55:05 +0300

    Add spec for rake task

---
+++ spec/rake_tasks/utils_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'rake'
+
+describe 'stats:fix_all'do
+  before do
+    Rake.application.rake_require 'tasks/utils'
+    Rake::Task.define_task(:environment)
+  end
+
+  subject(:task) { Rake::Task["reload_json"].invoke('fixtures/example.json') }
+
+  it 'creates data' do
+    expect { task }.to change(Bus, :count).by(1)
+                   .and(change(Trip, :count).by(10))
+                   .and(change(City, :count).by(2))
+                   .and(change(Service, :count).by(2))
+  end
+end
