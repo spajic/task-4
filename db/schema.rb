63826c579bb3039ef43b1a44c261ffdbcfe7d125
Stanislav Kravchenko <stankra@yandex.ru>
2019-05-05 11:38:53 +0300

    feat(Service): Add uniq index on name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_083024) do
+ActiveRecord::Schema.define(version: 2019_05_05_083758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_05_05_083024) do
 
   create_table "services", force: :cascade do |t|
     t.string "name"
+    t.index ["name"], name: "index_services_on_name", unique: true
   end
 
   create_table "trips", force: :cascade do |t|
